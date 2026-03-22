9275acfe01fedbf9b8061912daa36be6fcc33b75
Pratyaksh Gupta <pincodeazm@gmail.com>
2026-03-22 13:19:53 +0530

    cors error fix

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -3,4 +3,4 @@ import { type App } from "../app/api/[[...slugs]]/route";
 
 // .api to enter /api prefix
 // export const client = treaty<App>("localhost:3000").api;
-export const client = treaty<App>("chatterbox-v1").api;
+export const client = treaty<App>("chatterbox-v1.vercel.app").api;
